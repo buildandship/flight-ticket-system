f690f07e8faa7d4db3a9aa0749b6c3e16bf08abd
praveen <nice.prav@gmail.com>
2022-01-17 00:45:34 +0530

    17012022 00:45 Added the project structure and readme

---
+++ src/main/java/com/buildandship/flightticketsystem/FlightTicketSystemApplication.java
@@ -0,0 +1,14 @@
+package com.buildandship.flightticketsystem;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightTicketSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightTicketSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buildandship/flightticketsystem/FlightTicketSystemApplicationTests.java
@@ -0,0 +1,14 @@
+package com.buildandship.flightticketsystem;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightTicketSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
