519b8e37ffa1865d4dc4dc80b06bae5262960450
Praveen Mishra <nice.prav@gmail.com>
2022-03-22 13:43:41 +0530

    Added API gateway and fix config and discovery

---
+++ api-gateway-server/src/test/java/com/buildandship/airline/apigatewayserver/ApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildandship.airline.apigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
